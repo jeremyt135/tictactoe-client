c095fa4ed9d24ce3fa9039e8ec3a6d548c706c39
jtaylor-dev <jeremy.taylor.cs@gmail.com>
2022-03-07 18:34:28 -0800

    Update to use entry_points
    Replace "scripts" with "entry_points" for better cross platform support

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='jt.tictactoe',
-    version='1.0.0a',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'pysimplegui>=4.24.0'
     ],
-    scripts=['bin/tictactoe-client']
+    entry_points={
+        'console_scripts': [
+            'tictactoe-client=tictactoe.app:main',
+        ],
+    },
 )
